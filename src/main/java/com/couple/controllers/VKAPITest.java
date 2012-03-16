9b6fb23e51a9f700659493af18a0407bf00b898b
Oleksandr Shcherbyna <oleksandr_shcherbyna@epam.com>
2012-03-16 15:28:16 +0200

    CHange jelastic settings

--- src/main/java/com/couple/controllers/VKAPITest.java
+++ src/main/java/com/couple/controllers/VKAPITest.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class VKAPITest {
     @RequestMapping(value = "/vk", method = GET)
-    public @ResponseBody
-    String generateRecaptcha() {
-		return "<h1>Test</h1>";
+    public @ResponseBody String generateRecaptcha() {
+		return "<h1>Test!</h1>";
     }
 }
