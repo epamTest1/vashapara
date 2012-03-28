a035dd13d03f153da581acd936a0eab5eac2459d
Dmytro Lyash <Dmytro_Lyash@epam.com>
2012-03-28 11:57:41 +0300

    Controller/View intergation for second step

---
+++ src/main/java/com/couple/web/dto/User.java
@@ -0,0 +1,33 @@
+package com.couple.web.dto;
+
+public class User {
+	private String id;
+	private String name;
+	private String imageUrl;
+
+	public User(String id, String name, String imageUrl) {
+		this.id = id;
+		this.name = name;
+		this.imageUrl = imageUrl;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+}
