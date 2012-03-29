63e8b01d99c8740b05afcae06c2ec7204daf8b24
Dmytro Lyash <Dmytro_Lyash@epam.com>
2012-03-29 11:10:10 +0300

    In-memory DB (HSQL) introduced and used to load questions on the 2nd step

--- src/main/java/com/couple/persistence/stubs/CategoryInMemoryDao.java
+++ src/main/java/com/couple/persistence/stubs/CategoryInMemoryDao.java
@@ -3,25 +3,24 @@ package com.couple.persistence.stubs;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-
 import com.couple.model.Category;
 import com.couple.model.Question;
 import com.couple.persistence.CategoryDao;
 
-@Repository
+//@org.springframework.stereotype.Repository
 public class CategoryInMemoryDao implements CategoryDao {
 	private final List<Category> categories;
 	
+	
 	public CategoryInMemoryDao() {
-		Category home = new Category(1, "Дом");
-		home.addQuestion(new Question(1, "Убираю в квартире"));
-		home.addQuestion(new Question(2, "Мою посуду"));
-		home.addQuestion(new Question(3, "Выношу мусор"));
-		home.addQuestion(new Question(4, "Мою окна"));
+		Category home = new Category("Дом");
+		home.addQuestion(new Question("Убираю в квартире"));
+		home.addQuestion(new Question("Мою посуду"));
+		home.addQuestion(new Question("Выношу мусор"));
+		home.addQuestion(new Question("Мою окна"));
 		
-		Category finances = new Category(2, "Финансы");
-		finances.addQuestion(new Question(5, "бла-бла-бла"));
+		Category finances = new Category("Финансы");
+		finances.addQuestion(new Question("бла-бла-бла"));
 		
 		categories = Arrays.asList(home, finances);
 	}
---
+++ src/main/resources/db/initial-data.sql
@@ -0,0 +1,11 @@
+insert into CATEGORIES(ID, NAME) values(1, 'Дом')
+
+insert into QUESTIONS(CATEGORY_ID, TEXT) values(1, 'Убираю в квартире')
+insert into QUESTIONS(CATEGORY_ID, TEXT) values(1, 'Мою посуду')
+insert into QUESTIONS(CATEGORY_ID, TEXT) values(1, 'Выношу мусор')
+insert into QUESTIONS(CATEGORY_ID, TEXT) values(1, 'Мою окна')
+insert into QUESTIONS(CATEGORY_ID, TEXT) values(1, 'Test ;)')
+
+insert into CATEGORIES(ID, NAME) values(2, 'Финансы')
+
+insert into QUESTIONS(CATEGORY_ID, TEXT) values(2, 'Трачу все деньги )))))')
